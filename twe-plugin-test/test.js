1210f957dab076336c4dadf17d9e00dee887c0aa
星梦喵吖 <starsdream00@icloud.com>
2022-03-27 17:06:18 +0800

    更新示例插件

--- twe-plugin-test/test.js
+++ twe-plugin-test/test.js
@@ -1,6 +1,6 @@
 twe.registerPlugin("Test", "测试用插件", [1, 0, 1]);
 
-let chatId = 0;
+let chatId = 0; // 填你群组的ChatId
 
 let cache = [];
 let players = [];
@@ -8,49 +8,105 @@ twe.listen("ws.chat", (data) => {
     tg.sendMessage(chatId, `<${data.sender}> ${data.text}`);
 });
 twe.listen("ws.join", (data) => {
-    players.push(data.sender);
+    players.push(String(data.sender));
     tg.sendMessage(
         chatId,
         `${data.sender} 加入了服务器 当前在线${players.length}人`
     );
 });
 twe.listen("ws.left", (data) => {
-    let index = players.indexOf(data.sender);
-    if (index < 0) {
-        return;
+    let index = players.indexOf(String(data.sender));
+    if (index > -1) {
+        players.splice(index, 1);
+        tg.sendMessage(
+            chatId,
+            `${data.sender} 退出了服务器 当前在线${players.length}人`
+        );
     }
-    players.splice(index, 1);
-    tg.sendMessage(
-        chatId,
-        `${data.sender} 退出了服务器 当前在线${players.length}人`
-    );
 });
 twe.listen("ws.mobdie", (data) => {
     if (data.mobtype == "minecraft:player") {
-        let str = "";
-        switch (data.dmname) {
-            case "lava":
-                str += "被岩浆烧";
+        let type = "";
+        switch (String(data.dmname)) {
+            case "entity_attack":
+                type = `被${
+                    String(data.srcname) ? data.srcname : data.srctype
+                }杀死了`;
+                break;
+            case "projectile":
+                type = "被射杀";
                 break;
             case "entity_explosion":
-                str += `被 ${data.srctype} 炸`;
+                type = `被${
+                    String(data.srcname) ? data.srcname : data.srctype
+                }炸死了`;
+                break;
+            case "drowning":
+                type = "淹死了";
+                break;
+            case "fall":
+                type = "从高处摔了下来";
+                break;
+            case "lava":
+                type = "试图在熔岩里游泳";
+                break;
+            case "fire":
+                type = "浴火焚身";
+                break;
+            case "fire_tick":
+                type = "被烧死了";
+                break;
+            case "starve":
+                type = "饿死了";
+                break;
+            case "override":
+                type = "死了";
+                break;
+            case "thorns":
+            case "void":
+                type = "掉出了这个世界";
+                break;
+            case "fireworks":
+                type = "随着一声巨响消失了";
+                break;
+            case "magic":
+                type = `被${
+                    String(data.srcname) ? data.srcname : data.srctype
+                }使用的魔法杀死了`;
+                break;
+            case "anvil":
+                type = "被坠落的铁砧压扁了";
+                break;
+            case "magma":
+                type = "发现地面是熔岩";
+                break;
+            case "contact":
+                type = "被戳死了";
+                break;
+            case "lightning":
+                type = "被闪电击中";
+                break;
+            default:
+                type = data.dmname;
                 break;
         }
-        tg.sendMessage(chatId, `${data.mobname} ${str}死了`);
+        tg.sendMessage(chatId, `${data.mobname} ${type}`);
     }
 });
 twe.listen("tg.Message", (data) => {
     if (data.Message && data.Message.Chat.Id == chatId) {
-        if (
-            data.Message.Text == "/list" ||
-            data.Message.Text == `/list@${tg.bot.Username}`
-        ) {
-            cache.push([
-                mc.runcmd("list"),
-                (result) => {
-                    tg.sendMessage(chatId, result);
-                },
-            ]);
+        if (data.Message.Text.startsWith("/")) {
+            if (
+                data.Message.Text == "/list" ||
+                data.Message.Text == `/list@${tg.bot.Username}`
+            ) {
+                cache.push([
+                    mc.runcmd("list"),
+                    (result) => {
+                        tg.sendMessage(chatId, result);
+                    },
+                ]);
+            }
             return;
         }
         let date = new Date();
